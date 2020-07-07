98c86efb77a03596bc9a8e4ffabf2bf1a9279b1a
habib1234731 <habibrehman1234731@gmail.com>
2020-07-07 18:20:02 -0400

    added option to use multiple waypoints

--- src/App.js
+++ src/App.js
@@ -1,59 +1,77 @@
-
-import React, {useState} from 'react';
-import './App.css';
-import calculateDistance from './components/calculateDistance';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, Number, Button } from 'react-bootstrap'
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import calculateDistance from "./components/calculateDistance";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, Number, Button } from "react-bootstrap";
 
 function App() {
-  const [origin, setOrigin] = useState('')
-  const [dest, setDest] = useState([])
-  const [fields, setFields] = useState(0)
+  const [origin, setOrigin] = useState("");
+  const [fields, setFields] = useState(0);
   const [show, setShow] = useState(false);
+  const [waypoints, setWaypoints] = useState([]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  console.log(arr);
+  useEffect(() => {
+    if (show) {
+      let textFields = document.getElementById("textFields");
+      console.log(textFields);
+      for (let i = 0; i < fields; i++) {
+        let input = document.createElement("input");
+        input.type = "text";
+        input.name = "field" + i;
+        //if(typeof textFields === null) {
+        textFields.appendChild(input);
+        textFields.appendChild(document.createElement("br"));
+        textFields.appendChild(document.createElement("br"));
+        //   console.log(input)
+        // }
+        console.log(i + " " + input);
+      }
+    }
+  }, [show]);
 
   return (
     <div className="App">
-      <div id='firstDiv'>
-        <input id='initialTextField' onChange={e => {setFields(e.target.value)}}></input>
-        <button id='initialButton' onClick={() => handleShow()}>submit</button>
+      <div id="firstDiv">
+        <input
+          onChange={(e) => setOrigin(e.target.value)}
+          placeholder={"Origin"}
+        ></input>
+        <input
+          id="initialTextField"
+          onChange={(e) => {
+            setFields(e.target.value);
+          }}
+        ></input>
+        <button id="initialButton" onClick={() => handleShow()}>
+          submit
+        </button>
       </div>
 
-      <div id='secondDiv' >
-
+      <div id="secondDiv">
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            {(() => {
-              let ret;
-              
-              
-              for(let i = 0; i < fields; i++) {
-                let input = document.createElement("input");
-                input.type = "text";
-                input.name = "field"+i;
-                ret += input;
-              }
-              return ret
-            })()}
+            <div id="textFields"></div>                         //UNIQUE ID NAMES FOR EACH TEXT BOX
           </Modal.Body>
           <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+            <Button variant="secondary" onClick={handleClose}>
               Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => calculateDistance(origin, origin, waypoints)}
+            >
               Save Changes
-          </Button>
-          </Modal.Footer> 
-        </Modal>  
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-
     </div>
   );
 }
--- src/components/calculateDistance.js
+++ src/components/calculateDistance.js
@@ -1,16 +1,24 @@
 const google = window.google;
 
-export default function calculateDistance(origin, destination) {
-  let directionService = new google.maps.DirectionsService(),
-    request = {
-      origin: origin,
-      destination: destination,
-      travelMode: 'DRIVING'
-    }
-
-  directionService.route(request, (result, status) => {
-    if (status == 'OK') {
-      console.log(result);
-    }
-  })
-}
+export default function calculateDistance (origin, destination, waypts) {
+  let array = waypts.split(',');
+  let waypoints=[]
+  for (let i = 0; i < array.length;i++) {
+      waypoints.push({location: array[i], stopover:true})
+  }
+let directionService = new google.maps.DirectionsService(),
+  request = {
+    origin: origin,
+    destination: destination,
+    waypoints: waypoints,
+    optimizeWaypoints: true,
+    travelMode: "DRIVING",
+  };
+directionService.route(request, (result, status) => {
+  if (status == "OK") {
+    console.log(result);
+  } else {
+    window.alert("Directions request failed due to " + status);
+  }
+});
+};
